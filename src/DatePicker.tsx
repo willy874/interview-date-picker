160e9af30386a50c4e3ac6b5660d206a358942f0
willy <willy8742891@gmail.com>
2024-09-07 17:49:25 +0800

    feat: base DatePicker.

---
+++ src/DatePicker.tsx
@@ -0,0 +1,12 @@
+export default function DatePicker() {
+  return (
+    <div className="layout">
+      <div className="header">
+        <div className="month-select">{'<'}</div>
+        <div className="header__month">1</div>
+        <div className="month-select">{'>'}</div>
+      </div>
+      <div className="day-button day-state">1</div>
+    </div>
+  )
+}
